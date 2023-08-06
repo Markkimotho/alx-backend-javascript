b935655db7ac860d5dba59998e6eb3d8c5d16d90
Markkimotho <martinozas@gmail.com>
2023-08-06 15:55:40 +0300

    I promised to commit on Sunday, and I delivered

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
 export default function getResponseFromAPI() {
-  
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const responseData = {
+        message: 'Response from API',
+      };
+      resolve(responseData);
+    }, 2000);
+  });
 }
